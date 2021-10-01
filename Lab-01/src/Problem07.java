b8084efb7313539362898aafe709658f8f4eb3f8
eldiiar_b <eldiiar_b@auca.kg>
2021-10-01 15:15:16 +0300

    Problem05, Problem06, Problem07 completed URI1001, URI1004 attempted, not checked

---
+++ Lab-01/src/Problem07.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Problem07 {
+    public static void main(String[] args) {
+
+        System.out.println("a   a^2   a^3");
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(i + "   " + i * i +"     " + i * i * i);
+        }
+
+
+    }
+}
