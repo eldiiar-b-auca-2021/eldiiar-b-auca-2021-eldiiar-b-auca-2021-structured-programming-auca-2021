5d6bc35e18ef68ba52e7b323de8381bb62eb850f
eldiiar_b <eldiiar_b@auca.kg>
2021-10-17 15:08:53 +0300

    lab-02 Problem01

--- lab-02/src/Problem01.java
+++ lab-02/src/Problem01.java
@@ -14,6 +14,6 @@ public class Problem01 {
         int t = a;
         a = b;
         b = t;
-        System.out.printf("After swapping: a = %d; b = %d;\n", a, b);
+        System.out.printf("After swapping: a = %d; b = %d;\n",a, b);
     }
 }
