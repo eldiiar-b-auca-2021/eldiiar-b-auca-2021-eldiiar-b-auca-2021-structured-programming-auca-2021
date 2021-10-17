ffcfcc9f7e65082191455774105de1ac217d963f
eldiiar_b <eldiiar_b@auca.kg>
2021-10-17 17:05:28 +0300

    URI1016 accepted

---
+++ uri-midterm/src/URI1016.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class URI1016 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int X = inp.nextInt();
+
+        int b = X * 2;
+
+        System.out.println(b + " "+"minutos");
+    }
+}
