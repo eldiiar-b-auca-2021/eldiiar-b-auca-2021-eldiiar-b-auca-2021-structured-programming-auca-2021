ad745080c240ce9a34ca4a0553fa2dbd169d2f10
eldiiar_b <eldiiar_b@auca.kg>
2021-11-08 11:22:25 +0300

    lab-03, Problem06 completed

--- lab-03/src/Problem06.java
+++ lab-03/src/Problem06.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
 
 public class Problem06 {
-   public static void main(String[] args) {
-         Scanner inp = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
 
-         System.out.print("Enter a, b, c: ");
+        System.out.print("Enter a, b, c: ");
 
-         double a = inp.nextDouble();
-         double b = inp.nextDouble();
-         double c = inp.nextDouble();
-         double discriminant = b * b - 4 * a * c;
-         double root = (-b + Math.sqrt(discriminant));
-         double r = root / 2*a;
-         double secondRoot = (-b - Math.sqrt(discriminant));
-         double secondR = secondRoot / 2*a;
+        double a = inp.nextDouble();
+        double b = inp.nextDouble();
+        double c = inp.nextDouble();
+        double discriminant = b * b - 4 * a * c;
+        double root = (-b + Math.sqrt(discriminant));
+        double r = root / 2 * a;
+        double secondRoot = (-b - Math.sqrt(discriminant));
+        double secondR = secondRoot / 2 * a;
 
-       if (discriminant < 0) {
-           System.out.println("The equation has no real roots");
-       }
-       if (discriminant > 0) {
-           System.out.printf("The equation has two roots %f and %f\n",r,secondR);
-       }
-       if (discriminant == 0) {
-           double oneRoot = -b/2*a;
-           System.out.printf("The equation has one root %f\n",oneRoot);
-       }
-   }
+        if (discriminant == 0) {
+            double oneRoot = -b / 2 * a;
+             System.out.printf("The equation has one root %.0f\n", oneRoot);
+        } else if (discriminant > 0) {
+               System.out.printf("The equation has two roots %.6f and %.5f\n", r, secondR);
+        } else {
+                 System.out.println("The equation has no real roots");
+        }
+    }
 }
