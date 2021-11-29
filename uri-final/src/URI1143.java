5b4a52753a8a9346fdacff373a89ac65c3442467
eldiiar_b <eldiiar_b@auca.kg>
2021-11-29 11:23:02 +0300

    uri-final, URI1143 accepted

---
+++ uri-final/src/URI1143.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class URI1143 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int input = inp.nextInt();
+
+        for (int i = 1; i <= input; i++) {
+            System.out.println( i + " " + i * i + " " + i * i * i);
+
+            }
+        }
+    }
