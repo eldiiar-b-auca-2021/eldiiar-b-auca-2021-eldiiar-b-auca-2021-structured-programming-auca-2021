42822ca2136603a473398e2334b3e29172c0530e
eldiiar_b <eldiiar_b@auca.kg>
2021-11-29 12:01:13 +0300

    uri-final, URI1146 time limit exceeded2

--- uri-final/src/URI1146.java
+++ uri-final/src/URI1146.java
@@ -4,14 +4,14 @@ public class URI1146 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
 
-        int anyNum = 1;
+        int anyNum;
 
-        while (anyNum != 0) {
+        do  {
             anyNum = inp.nextInt();
             for (int i = 1; i <= anyNum; i++) {
                 System.out.print(i + " ");
             }
 
-        }
+        } while (anyNum != 0);
     }
 }
