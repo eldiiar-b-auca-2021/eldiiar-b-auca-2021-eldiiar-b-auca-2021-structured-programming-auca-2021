80ec7cf8992a03578865876edef9fdd1c6a19845
eldiiar_b <eldiiar_b@auca.kg>
2021-12-03 22:51:40 +0300

    lab-05, Problem05 is corrected

--- lab-05/src/Problem05.java
+++ lab-05/src/Problem05.java
@@ -1,7 +1,7 @@
 import processing.core.*;
 
 public class Problem05 extends PApplet {
-    static final int NUM_OF_CIRCLES = 50;
+    static final int NUM_OF_CIRCLES = 16;
     public void settings() {
         fullScreen();
     }
@@ -15,7 +15,7 @@ public class Problem05 extends PApplet {
         float r = min(height,  width) / 2f;
         float redComp = 5;
         float dr = r / NUM_OF_CIRCLES;
-        float dRedComp = 255 / NUM_OF_CIRCLES;
+        float dRedComp = 255f / NUM_OF_CIRCLES;
 
         for (int i = 0; i < NUM_OF_CIRCLES; i++) {
             fill(redComp,0,0);
