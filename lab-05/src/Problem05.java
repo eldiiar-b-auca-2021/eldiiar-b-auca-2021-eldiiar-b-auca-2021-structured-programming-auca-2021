897e0b78474ff50ad3c6f9a7b3ff84542455cbb9
eldiiar_b <eldiiar_b@auca.kg>
2021-12-03 22:25:27 +0300

    lab-05, Problem05 is completed

---
+++ lab-05/src/Problem05.java
@@ -0,0 +1,35 @@
+import processing.core.*;
+
+public class Problem05 extends PApplet {
+    static final int NUM_OF_CIRCLES = 50;
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+    noStroke();
+    }
+
+    public void draw() {
+        background(0);
+        float r = min(height,  width) / 2f;
+        float redComp = 5;
+        float dr = r / NUM_OF_CIRCLES;
+        float dRedComp = 255 / NUM_OF_CIRCLES;
+
+        for (int i = 0; i < NUM_OF_CIRCLES; i++) {
+            fill(redComp,0,0);
+            circle (width / 2f, height / 2f, 2 * r);
+            r -= dr;
+            redComp += dRedComp;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Problem05");
+    }
+
+}
+
