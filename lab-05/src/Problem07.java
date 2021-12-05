c2590e25ee45ca97ee017fb69f2b743d000ba201
eldiiar_b <eldiiar_b@auca.kg>
2021-12-05 16:16:08 +0300

    lab-05, Problem07 completed

---
+++ lab-05/src/Problem07.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Problem07 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+
+
+        int sum = 0;
+        int primaryNum = 0;
+        while (primaryNum < 20) {
+            primaryNum++;
+            sum+=primaryNum;
+
+            if (sum >= 100) {
+               break;
+            }
+          
+        }
+        System.out.println("The number is " + primaryNum);
+        System.out.println("The sum is " + sum);
+
+
+    }
+}
