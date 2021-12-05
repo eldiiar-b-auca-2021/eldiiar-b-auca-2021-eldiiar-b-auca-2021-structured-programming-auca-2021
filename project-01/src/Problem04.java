70c7aca1882c691cd13cf49cd49d18f3c6b31a6f
eldiiar_b <eldiiar_b@auca.kg>
2021-12-05 17:19:58 +0300

    project-01, Problem04 is in the process

--- project-01/src/Problem04.java
+++ project-01/src/Problem04.java
@@ -11,7 +11,7 @@ public class Problem04 extends PApplet {
     }
 
     public void setup() {
-         //stroke(0,0,255);
+        textSize(30);
         circleX = 1000 / 2f;
         circleY = 800 / 2f;
         circleR = 800 / 20f;
@@ -19,18 +19,25 @@ public class Problem04 extends PApplet {
     }
 
     public void draw() {
-        background(0,0,0);
-        stroke(0,0,255);
+        fill(0,255,0);
+       text ("Game: Collect Yellow Circles using Arrow Buttons",200,50);
+       text("Score: ", 400, 750);
 
+        stroke(0,0,255);
 
         for (int i = 0; i < 550; i+=40) {
 
             fill(255, 0, 0);
-            line(100, 100 + i, width - 100 , height - 700 + i);
+            line(120, 100 + i, width - 120 , height - 700 + i);
         }
-        line(100, 600, width - 100, height-200);
+        for(int j = 0; j < 800; j+=40) {
+            fill (255,0,0);
+            line(121+j,100,width-879+j,height-180);
+        }
 
-        fill(0,0,0,50);
+        //line(100, 600, width - 100, height-200);
+        noStroke();
+        fill(0,0,0,100);
         rect(0,0,width,height);
 
         fill(255,0,0);
