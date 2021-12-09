57c8802b09bc573d8a99da1b03c3596a061ab4ee
eldiiar_b <eldiiar_b@auca.kg>
2021-12-09 18:45:04 +0300

    project-01, Problem02 is in process

--- project-01/src/Problem02.java
+++ project-01/src/Problem02.java
@@ -21,8 +21,8 @@ public class Problem02 extends PApplet {
         x3 = (width/2f-35f);
         y3 = (height/2f-35f);
 
-        speedX1 = random(-8,8);
-        speedY1 = random(-8,8);
+        speedX1 = random(-10,10);
+        speedY1 = random(-10,10);
         speedX2 = speedX1;
         speedY2 = speedY1;
         speedX3 = speedX1;
@@ -41,6 +41,65 @@ public class Problem02 extends PApplet {
 
         fill(0,0,255);
         circle (x2,y2,50);
+
+        x1 += speedX1;
+        y1 += speedY1;
+
+        x2 += speedX2;
+        y2 += speedY2;
+
+        x3 += speedX3;
+        y3 += speedY3;
+
+
+        if (x1 <= 0){
+            x1 = 1;
+            speedX1 = -speedX1;
+        }
+        if (y1 <= 0) {
+            y1 = 1;
+            speedY1 = -speedY1;
+        }
+        if (x1 >= width) {
+            x1 = width-1;
+            speedX1 = -speedX1;
+        }
+        if (y1 >= height) {
+            y1 = height - 1;
+            speedY1 = -speedY1;
+        }
+        if (x2 <= 0) {
+            x2 = 1;
+            speedX2 = -speedX2;
+        }
+        if (y2 <= 0) {
+            y2 = 1;
+            speedY2 = -speedY2;
+        }
+        if (x2 >= width) {
+            x2 = width - 1;
+            speedX2 = -speedX2;
+        }
+        if (y2 >= height) {
+            y2 = height - 1;
+            speedY2 = -speedY2;
+        }
+        if (x3 <= 0) {
+            x3 = 1;
+            speedX3 = -speedX3;
+        }
+        if (y3 <= 0) {
+            y3 = 1;
+            speedY3 = -speedY3;
+        }
+        if (x3 >= width) {
+            x3 = width-1;
+            speedX3 = -speedX3;
+        }
+        if (y3 >= height) {
+            y3 = height -1;
+            speedY3 = -speedY3;
+        }
 
     }
 
