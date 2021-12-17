2194fb4cf56c4150419a94054526f5c4a4896cce
eldiiar_b <eldiiar_b@auca.kg>
2021-12-17 20:30:32 +0300

    project-01, Problem06 is completed

--- project-01/src/Problem06.java
+++ project-01/src/Problem06.java
@@ -1,152 +1,162 @@
 import processing.core.*;
 public class Problem06 extends PApplet {
-    float alpha,beta,gamma;
+    float alpha;
+    float beta ;
+    float gamma;
+    float red;
+    float green;
+    float blue;
+    float radiusChanger;
+    float radiusOfSun;
+    float radiusOfVenus;
+    float radiusOfEarth;
+    float radiusOfMoon;
     public void settings() {
-
         fullScreen();
     }
     public void setup() {
         textSize(30);
         alpha = 1;
-        beta =  0;
-        gamma = 1f;
+        beta = 0;
+        gamma = 1;
     }
-
     public void draw() {
         background(0,0,0);
-        fill(0, 0, 255);
-        rect(width - 300, 50, 200, 50);
-        fill(255,255,255);
-        text ("Sun", width - 250, 50,200,50);
-        fill(0,0,255);
-        stroke(255);
-        rect (width - 300, 100, 200,50);
-        fill(255,255,255);
-        text ("Venus",width -250, 100,200,50);
-        fill(0,0,255);
-        rect (width - 300, 150, 200,50);
-        fill(255,255,255);
-        text ("Earth",width -250,150,200,50);
-        fill(0,0,255);
-        rect(width-300,200,200,50);
-        fill(255,255,255);
-        text("Moon", width -250,200,200,50);
-        if (mouseX >= width - 300 && mouseX <= width - 100 &&
-                mouseY >= 50 && mouseY <= 99) {
-            fill(255,0,0);
-            text ("Sun", width-250, 50,200,50);
-        }
+        float firstPointOfWidth = width-250;
+        float firstPointOfHeight = 50;
 
-        if (mouseX >= width - 300 && mouseX <= width - 100 && mouseY >= 200 && mouseY <= 250) {
+
+        fill(0,0,255);
+        rect(firstPointOfWidth,firstPointOfHeight,200,50);
+        rect(firstPointOfWidth,100,200,50);
+        rect(firstPointOfWidth,150,200,50);
+        rect(firstPointOfWidth,200,200,50);
+
+
+        fill(255,255,255);
+        text ("SUN",firstPointOfWidth+50,firstPointOfHeight,200,50);
+        text("VENUS", firstPointOfWidth+50,100,200,100);
+        text("EARTH", firstPointOfWidth+50,150,200,150);
+        text("MOON", firstPointOfWidth+50,200,200,200);
+
+        if (mouseX >= firstPointOfWidth && mouseX <= width - 50 &&
+                mouseY >= firstPointOfHeight && mouseY <= 99) {
             fill(255,0,0);
-            text("Moon", width -250,200,200,50);
-        }
-        if (mouseX >= width-300 && mouseX <= width - 100 && mouseY >= 150 && mouseY <= 199) {
-            fill(255, 0, 0);
-            text("Earth", width-250,150,width-100,199);
+            text ("SUN",firstPointOfWidth+50,firstPointOfHeight,200,50);
             noFill();
             stroke(255,0,0);
             strokeWeight(1);
-            circle(width/2f,height/2f,700);
+            circle(width/2f,height/2f,302);
         }
-        if (mouseX >= width-300 && mouseX <= width-100 && mouseY >= 100 && mouseY <= 149 ) {
+        if (mouseX >= firstPointOfWidth && mouseX <= width -50 &&
+                mouseY >= 100 && mouseY <= 149) {
             fill(255,0,0);
-            text ("Venus",width -250, 100,200,50);
+            text("VENUS", firstPointOfWidth+50,100,200,100);
             noFill();
-            stroke (255,0,0);
+            stroke(255,0,0);
             strokeWeight(1);
-            circle(width/2f, height /2f, 480);
+            circle(width/2f,height/2f,510);
         }
-
+        if (mouseX >= firstPointOfWidth && mouseX <= width - 50 &&
+                mouseY >= 150 && mouseY <= 199) {
+            fill(255,0,0);
+            text("EARTH", firstPointOfWidth+50,150,200,150);
+            noFill();
+            stroke(255,0,0);
+            strokeWeight(1);
+            circle(width/2f,height/2f,750);
+        }
+        if (mouseX >= firstPointOfWidth && mouseX <= width - 50 &&
+                mouseY >= 200 && mouseY <= 250) {
+            fill(255,0,0);
+            text("MOON", firstPointOfWidth+50,200,200,200);
+        }
         pushMatrix();
-        translate(width / 2f, height / 2f);
-        float rad1 = 300f, d = 5f, red = 0, green = 0, dr = 5f;
+        translate(width/2f,height/2f);
+        red = 0;
+        green = 0;
+        blue = 0;
+        radiusChanger = 5;
+        radiusOfSun = 300;
+        radiusOfVenus = 100;
+        radiusOfEarth = 95;
+        radiusOfMoon = 20;
         noStroke();
         for (int i = 0; i < 30; i++) {
-            fill(red, green, 0);
-            circle(0, 0, rad1);
-            rad1 -= d;
-            red += dr;
-            green += dr;
-        }
-        if (mouseX >= width - 300 && mouseX <= width - 100 &&
-                mouseY >= 50 && mouseY <= 99) {
-            noFill();
-            stroke(255,0,0);
-            strokeWeight(1);
-            circle(0,0,300);
+            fill(red,green,0);
+            circle(0,0,radiusOfSun);
+            red += radiusChanger;
+            green += radiusChanger;
+            radiusOfSun -= radiusChanger;
         }
         rotate(alpha);
-        alpha += 0.02f;
-
-
-        float green3 = 0, blue3 = 0, venusChanger = 5;
-        float radiusOfVenus = 100f, ch = 3;
+        alpha+=0.01;
+        float rad2 = 3, color2 = 5, green2 = 0, blue2 = 0, radiusOfVenus2 = 100;
         noStroke();
         for (int i = 0; i < 30; i++) {
-            fill(0,green3,blue3);
-            circle(230, 0, radiusOfVenus);
-            green3+=venusChanger;
-            blue3+=venusChanger;
-            radiusOfVenus-=ch;
+            fill(0,green2,blue2);
+            circle(250,0,radiusOfVenus2);
+            green2 += color2 ;
+            blue2 += color2;
+            radiusOfVenus2 -= rad2;
         }
-        if (mouseX >= width-300 && mouseX <= width-100 && mouseY >= 100 && mouseY <= 149 ) {
+        if (mouseX >= firstPointOfWidth && mouseX <= width -50 &&
+                mouseY >= 100 && mouseY <= 149) {
             noFill();
-            stroke(255,0,0);
+            stroke(255, 0, 0);
             strokeWeight(1);
-            circle(230,0,100);
+            circle(250, 0, 100);
         }
         popMatrix();
-
-
 
         pushMatrix();
-        translate (width / 2f, height /2f);
+        translate(width/2f,height/2f);
         rotate(beta);
-        float blue = 0, change = 5;
-        float radius = 95f, radiusChange = 3f;
+        beta+=0.01;
+        float rad3 = 3, blue3 = 0, color = 5,radiusOfEarth2 = 95;
         noStroke();
         for (int i = 0; i < 30; i++) {
-            fill(0,0, blue);
-            circle(350, 0, radius);
-            blue+=change;
-            radius-=radiusChange;
+            fill(0,0,blue3);
+            circle (380,0,radiusOfEarth2);
+            blue3 += color;
+            radiusOfEarth2 -= rad3;
         }
-        if (mouseX >= width-300 && mouseX <= width - 100 && mouseY >= 150 && mouseY <= 199) {
-            noFill();
-            stroke (255,0,0);
-            strokeWeight(1);
-            circle(350, 0, 95);
-        }
-        beta += 0.02;
-        translate(350,0);
-        rotate(gamma);
-        float red1 = 0, green1 = 0, blue1 = 0, colorChanger = 5;
-        float moonRadius = 10f, radiusChanger = 1;
-        noStroke();
-        for (int i = 0; i < 30; i++) {
-            fill (red1,green1,blue1);
-            circle(80, 0, moonRadius);
-            red1+=colorChanger;
-            green1 += colorChanger;
-            blue1 += colorChanger;
-            moonRadius-=radiusChanger;
-        }
-        if (mouseX >= width - 300 && mouseX <= width - 100 && mouseY >= 200 && mouseY <= 250) {
+        if (mouseX >= firstPointOfWidth && mouseX <= width - 50 &&
+                mouseY >= 150 && mouseY <= 199) {
             noFill();
             stroke(255,0,0);
-            strokeWeight(2);
-            circle (80,0,25);
+            strokeWeight(1);
+            circle(380,0,97);
         }
-        gamma += 0.05;
+        translate(380,0);
 
+
+        float rad4 = 1,radiusOfMoon = 30, red4 = 0, green4 = 0, blue4 = 0, color4 = 1f;
+        rotate(gamma);
+        gamma += 0.05f;
+        noStroke();
+        for (int i = 0; i < 30; i++) {
+            fill(red4,green4,blue4);
+            circle(60,0,radiusOfMoon);
+            red4+=color4;
+            green4+=color4;
+            blue4+=color4;
+            radiusOfMoon-=rad4;
+        }
+        if (mouseX >= firstPointOfWidth && mouseX <= width - 50 &&
+                mouseY >= 200 && mouseY <= 250) {
+            noFill();
+            stroke(255,0,0);
+            strokeWeight(1);
+            circle(60,0,33);
+            circle(0,0,130);
+        }
 
         popMatrix();
-
     }
+
 
     public static void main(String[] args) {
         PApplet.main("Problem06");
     }
-
 }
