4227fb6aa3d6d2775371c8e149da3d3d8062e829
eldiiar_b <eldiiar_b@auca.kg>
2021-12-19 17:13:35 +0300

    uri-final, URI1097 accepted

---
+++ uri-final/src/URI1097.java
@@ -0,0 +1,18 @@
+public class URI1097 {
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 6;
+        int c = 5;
+        for (int i = 1; i < 10; i++) {
+            System.out.println("I=" + i + " J=" + a);
+            System.out.println("I=" + i + " J=" + b);
+            System.out.println("I=" + i + " J=" + c);
+            a+=2;
+            b+=2;
+            c+=2;
+            i+=1;
+            }
+        }
+    }
+
