84d6c9f531797d84ab059d7c72b12d8964910a35
eldiiar_b <eldiiar_b@auca.kg>
2021-12-19 19:54:51 +0300

    lab-07, Problem02 completed

--- lab-06/src/Problem02.java
+++ lab-06/src/Problem02.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Problem02 {
     static Scanner inp = new Scanner(System.in);
     public static void main(String[] args) {
-
-        int year = readNumber("Year: ", "Incorrect number of year ", 1, 100000);
-        int month = readNumber("Month: ", "Incorrect number of month ", 1, 12);
-        int days = getNumberOfDays(year, month );
-        System.out.println("Number of days: " + days);
+        while (true) {
+            int year = readNumber("Year: ", "Incorrect number of year ", 1, 100000);
+            int month = readNumber("Month: ", "Incorrect number of month ", 1, 12);
+            int days = getNumberOfDays(year, month);
+            System.out.println("Number of days: " + days);
+        }
     }
     static int readNumber (String msg, String errorMsg, int beg, int end) {
 
--- lab-07/src/Problem02.java
+++ lab-07/src/Problem02.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 public class Problem02 {
     static Scanner inp = new Scanner(System.in);
     public static void main(String[] args) {
-
-        int year = readNumber("Year: ", "Incorrect number of year ", 1, 100000);
-        int month = readNumber("Month: ", "Incorrect number of month ", 1, 12);
-        int days = getNumberOfDays(year, month );
-        System.out.println("Number of days: " + days);
+        while (true) {
+            int year = readNumber("Year: ", "Incorrect number of year ", 1, 100000);
+            int month = readNumber("Month: ", "Incorrect number of month ", 1, 12);
+            int days = getNumberOfDays(year, month);
+            System.out.println("Number of days: " + days);
+        }
     }
     static int readNumber (String msg, String errorMsg, int beg, int end) {
-
         System.out.print(msg);
+        if (!inp.hasNextInt()) {
+            System.exit(0);
+        }
         int res = inp.nextInt();
         if (res < beg || end < res) {
             System.out.println(errorMsg+res);
