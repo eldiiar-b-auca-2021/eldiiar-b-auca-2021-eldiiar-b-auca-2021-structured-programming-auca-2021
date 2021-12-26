07418f0a7a581e53e8829440ba03a1244f9c8905
eldiiar_b <eldiiar_b@auca.kg>
2021-12-26 20:41:18 +0300

    project-01, Problem05 is in the process

--- project-01/src/Problem05.java
+++ project-01/src/Problem05.java
@@ -5,17 +5,19 @@ import javax.swing.*;
 public class Problem05 extends PApplet {
     static final float BOARD_SIZE = 600;
     float radius;
-    float SIZE = 50;
-    float rectX;
-    float rectY;
+    float row,x;
+    float column,y;
+    String color;
     public void settings() {
-
         fullScreen();
     }
-
-
     public void setup() {
-
+        textSize(30);
+        x = width/2f-300f;
+        y = height/2f -300f;
+        row = -1;
+        column = -1;
+        color = "No Color";
         try {
             String strRadius = JOptionPane.showInputDialog("Radius [10, 300]: ");
             radius = Float.parseFloat(strRadius);
@@ -31,31 +33,42 @@ public class Problem05 extends PApplet {
     }
     public void draw() {
         background(0,0,0);
-        translate(width/2f-300f, height/2f - 300);
+        fill(255,255,0);
+        text("Row:"+row+" Column: "+column+"  Color: "+color,width/2f-200,50);
         fill(0,0,0);
         stroke (255,255,255);
-        float x = 0,y = 0;
         rect(x, y, BOARD_SIZE, BOARD_SIZE);
-        float x1,y1;
+
         for (int i = 0; i < radius; i++) {
            for (int j = 0; j < radius;j++){
-               x1 = i * BOARD_SIZE/radius;
-               y1 = j * BOARD_SIZE/radius;
+               float x1 = x + i * BOARD_SIZE/radius;
+               float y1 = y + j * BOARD_SIZE/radius;
                if ((i+j) % 2 == 0) {
-                   fill(255,255,255);
+                   fill(150,150,150);
                } else {
-                   fill (0,255,0);
+                   fill (0,30,0);
+               }
+               if (mouseX >= x1 && mouseX <= x1+BOARD_SIZE/radius && mouseY >= y1 && mouseY <= y1+BOARD_SIZE/radius) {
+                   row = i;
+                   column = j;
+                   if ((i+j) % 2 != 0) {
+                       color = "Black";
+                   } else {
+                       color = "White";
+                   }
+                   noFill();
+                   square(x1,y1,BOARD_SIZE/radius);
                }
               square(x1,y1,BOARD_SIZE/radius);
            }
-        }
 
+        }
+        fill(255, 0,0);
+        ellipse(mouseX,mouseY,1,1);
 
     }
-
     public static void main(String[] args) {
         PApplet.main("Problem05");
 
     }
-
 }
